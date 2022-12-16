2937c73d01a6c40b2e5cad6b01e09c09fc4054c0
madhusrigarlapati <20pa1a1234@vishnu.edu.in>
2022-12-16 22:02:47 +0530

    Functional HTTP Server

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
-console.log("Hello Node.js!");
+const http = require("http");
+const fs = require("fs");
+
+let homeContent = "";
+let projectContent = "";
+let registrationContent = "";
+let args= require("minimist")(process.argv.slice(2));
+let port=args["port"];
+fs.readFile("home.html", (err, home) => {
+  if (err) {
+    throw err;
+  }
+  homeContent = home;
+});
+
+fs.readFile("project.html", (err, project) => {
+  if (err) {
+    throw err;
+  }
+  projectContent = project;
+});
+
+fs.readFile("registration.html", (err, registration) => {
+    if (err) {
+      throw err;
+    }
+    registrationContent = registration;
+  });
+
+fs.readFile("home.html", (err, home) => {
+    if (err) {
+      throw err;
+    }
+    http.createServer((request, response) => {
+    let url = request.url;
+    response.writeHeader(200, { "Content-Type": "text/html" });
+    switch (url) {
+      case "/project":
+        response.write(projectContent);
+        response.end();
+        break;
+      case "/project/registration":
+        response.write(registrationContent);
+        response.end();
+        break;
+      default:
+        response.write(homeContent);
+        response.end();
+        break;
+    }
+  })
+  .listen(port);
+  });
