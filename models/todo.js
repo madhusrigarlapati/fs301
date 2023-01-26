dca4310f8c940edb62e066291b1d6568eb69b716
madhusrigarlapati <20pa1a1234@vishnu.edu.in>
2023-01-26 19:15:08 +0530

    Todo using postgre and Sequelize l5

--- models/todo.js
+++ models/todo.js
@@ -1,5 +1,30 @@
 /* eslint-disable no-unused-vars */
-
+// /* eslint-disable no-unused-vars */
+// 'use strict';
+// const {
+//   Model
+// } = require('sequelize');
+// module.exports = (sequelize, DataTypes) => {
+//   class Todo extends Model {
+//     /**
+//      * Helper method for defining associations.
+//      * This method is not a part of Sequelize lifecycle.
+//      * The `models/index` file will call this method automatically.
+//      */
+//     static associate(models) {
+//       // define association here
+//     }
+//   }
+//   Todo.init({
+//     title: DataTypes.STRING,
+//     dueDate: DataTypes.DATEONLY,
+//     completed: DataTypes.BOOLEAN
+//   }, {
+//     sequelize,
+//     modelName: 'Todo',
+//   });
+//   return Todo;
+// };
 
 // models/todo.js
 'use strict';
